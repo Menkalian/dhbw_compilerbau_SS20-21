a33397fffbf36eef85c55f4b29f8684241d9ec6b
Kilian Krampf <kilian@krampf.de>
2021-02-08 11:31:12 +0100

    Add code-skeleton for the visitors

---
+++ src/main/java/de/dhbw/mosbach/compilerbau/FollowPosTableGenerator.java
@@ -0,0 +1,22 @@
+package de.dhbw.mosbach.compilerbau;
+
+import de.dhbw.mosbach.compilerbau.ast.BinOpNode;
+import de.dhbw.mosbach.compilerbau.ast.OperandNode;
+import de.dhbw.mosbach.compilerbau.ast.UnaryOpNode;
+
+public class FollowPosTableGenerator implements Visitor {
+    @Override
+    public void visit (OperandNode node) {
+        // TODO: 08.02.2021 (...)
+    }
+
+    @Override
+    public void visit (BinOpNode node) {
+        // TODO: 08.02.2021 (...)
+    }
+
+    @Override
+    public void visit (UnaryOpNode node) {
+        // TODO: 08.02.2021 (...)
+    }
+}
