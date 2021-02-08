e77b1a93dcf88ae30aa99d6dca770760993f6e55
Kilian Krampf <kilian@krampf.de>
2021-02-08 11:58:30 +0100

    Add dummy test to prevent pipeline from failing

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+    // THIS IS NEEDED FOR CI (does not work without any tests)
+    @Test
+    public void testTestsRunning () {
+        Assertions.assertTrue(true);
+    }
+}
